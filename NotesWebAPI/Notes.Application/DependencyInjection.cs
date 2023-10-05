3b7811b30e202fb571654893b1eff4f4b69e9a84
John <wesatellite@bigmir.net>
2023-10-05 13:14:08 +0300

    DependencyInjection
    Add and Register DbContext
    Add and Register MediatR

---
+++ NotesWebAPI/Notes.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using System.Reflection;
+
+namespace Notes.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
