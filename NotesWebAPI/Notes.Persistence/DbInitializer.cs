bcc413e2c3eb5142746d517c1f486b1adb7cd383
John <wesatellite@bigmir.net>
2023-10-05 12:37:38 +0300

    Add EntityFramework Core
    Infrastructure Folder.
    
    1. Install EntityFrameworkCore.Sqlite to Notes.Persistence project
    2. Create Folder EntityTypeConfigurations/NoteConfiguration.cs to reduce the size of OnModelCreating method and make a conceptual division
    
    Core Folder.
    (To make Context DataBase)
    Notes.Notes.Application project:
    Create Folder Interfaces/INotesDbContext.cs
    
    Infrastructure Folder.
    Notes.Persistence project:
    Create NotesDbContext
    Create DbInitializer with method Initialize - checks if DataBase is created, if no - DataBase will be created based on our context.

---
+++ NotesWebAPI/Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+
+    }
+}
